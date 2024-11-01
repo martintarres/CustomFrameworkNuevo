a9ad4d93cd2c8a6e98c46f53cf3cc7b45d94cf0e
Martin Tarres <martintarres@gmail.com>
2024-11-01 14:10:58 -0300

    Initial Commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Martin Tarres on 01/11/2024.
+//
+
+import Foundation
+
+public struct SDK{
+    public static func doSomework(){
+        print("Doing some work")
+    }
+}
